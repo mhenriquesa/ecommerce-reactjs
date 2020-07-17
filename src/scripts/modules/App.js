37f0ce00a725d6808de4a843b48ac345b1adbc3e
Henrique S.A <henriqueator@gmail.com>
2020-07-17 17:11:15 -0300

    Inital structure - Homepage component - Homepage styles

---
+++ src/scripts/modules/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Homepage from '../components/homepage.component.jsx';
+
+const App = () => {
+  return (
+    <div className="">
+      <Homepage />
+    </div>
+  );
+};
+
+export default App;
