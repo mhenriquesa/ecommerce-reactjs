fbe5f54213d01237169df44e2ffb1b92d4e6148d
Moises Henrique SA <henriqueator@gmail.com>
2020-07-18 14:40:07 -0300

    Visual effects 's grid - Defined font family Open sans condensed

--- src/scripts/components/menu-item.component.jsx
+++ src/scripts/components/menu-item.component.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 const MenuItem = ({ title, image, size }) => {
   return (
-    <div className={`menu-item ${size}`} style={{ backgroundImage: `url(${image})` }}>
+    <div className={`menu-item ${size}`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${image})` }}
+      ></div>
+
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">Shop now</span>
